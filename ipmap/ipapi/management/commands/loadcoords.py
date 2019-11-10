bd8501c25db6588d2fc0f355f23f010fe937f7f7
Eric Petway <eric.petway+alt@gmail.com>
2019-11-10 16:44:08 -0500

    added `IPCoord` model and `loadcoords` management command to load in csv data.

---
+++ ipmap/ipapi/management/commands/loadcoords.py
@@ -0,0 +1,65 @@
+import csv
+from collections import defaultdict
+from decimal import Decimal
+
+from tqdm import tqdm
+
+from django.core.management.base import BaseCommand, CommandError
+
+from ipapi.models import IPCoord
+
+
+class Command(BaseCommand):
+    help = "Load GeoLite IP address data"
+
+    def add_arguments(self, parser):
+        parser.add_argument("csvfile", type=str, help="GeoLite csv file")
+
+    def load_data(self, csvfile):
+        row_count = 0
+        self.stdout.write("Loading file...")
+        with open(csvfile, "r") as infile:
+            reader = csv.DictReader(infile)
+            row_count = 0
+            # TODO: might be nice to have rotating progress marker here
+            # REVIEW: I know it adds a bit of processing time to get the
+            # row count, but it is nice to have a total progress bar later
+            # for the actual file processing.
+            self.stdout.write("Getting row count...")
+            for row in reader:
+                row_count += 1
+            infile.seek(0)
+            coord_counts = defaultdict(int)
+            next(reader)
+            for row in tqdm(
+                reader, total=row_count, desc="Processing file...", unit="rows "
+            ):
+                coord_counts[(row["latitude"], row["longitude"])] += 1
+            created_count = 0
+            updated_count = 0
+            for coord, count in tqdm(
+                coord_counts.items(),
+                total=len(coord_counts),
+                desc="Updating database...",
+            ):
+                ipcoord, created = IPCoord.objects.update_or_create(
+                    latitude=Decimal(coord[0]),
+                    longitude=Decimal(coord[1]),
+                    defaults={"count": count},
+                )
+                if created:
+                    created_count += 1
+                else:
+                    updated_count += 1
+        if created_count:
+            self.stdout.write(
+                self.style.SUCCESS(f"Created {created_count} coordinates.")
+            )
+        if updated_count:
+            self.stdout.write(
+                self.style.SUCCESS(f"Updated {updated_count} coordinates.")
+            )
+        self.stdout.write(style.SUCCESS("All Done!"))
+
+    def handle(self, *args, **options):
+        self.load_data(options.get("csvfile"))
--- ipmap/ipapi/models.py
+++ ipmap/ipapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class IPCoord(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    count = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = ["latitude", "longitude"]
